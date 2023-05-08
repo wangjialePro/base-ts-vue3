f98b3f000254f0415985475cee8e95115238cb38
wangjiale <wangjiale@sansi.com>
2023-05-08 16:54:48 +0800

    feat(*): init

--- preview/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
-import router from './router'
-import "@/util/rem"
-createApp(App).use(router).use(store).mount('#app')
